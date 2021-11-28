0a8c706f4160dc9f371952b49d808a305fba997c
Mike Zamayias <mzamagias@gmail.com>
2021-11-28 18:16:10 +0200

    refactored

---
+++ lib/src/clay_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ClayList extends StatefulWidget {
+  const ClayList({Key? key}) : super(key: key);
+
+  @override
+  _ClayListState createState() => _ClayListState();
+}
+
+class _ClayListState extends State<ClayList> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
